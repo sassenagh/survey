5a7db2cdb90489c75b8a34560a4b4ff0304eb98d
sassenagh <anna.alcaide.hijano@gmail.com>
2020-06-06 13:57:49 +0200

    change greeting

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require ('express');
-const app = express ();
+const express = require('express');
+const app = express();
 
-app.get ('/', (req, res) => {
-  res.send ({hi: 'there'});
+app.get('/', (req, res) => {
+  res.send({hi: 'yeah'});
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen (PORT);
+app.listen(PORT);
